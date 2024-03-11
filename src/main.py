da134ef30906be5cb67dccdbf3d465cee238d85d
lukasz <lukasz@MacBook-Pro-lukasz.local>
2024-03-11 21:39:59 +0100

    minor change

--- src/main.py
+++ src/main.py
@@ -15,4 +15,4 @@ def main(request):
     elif request_json and 'message' in request_json:
         return request_json['message']
     else:
-        return f'Hello World!'
+        return f'Hello World! my world'
